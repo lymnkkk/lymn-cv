8dc7f871bced3e9f21e0d3658ca083abd3d38a6c
lymnkkk <843214245@qq.com>
2018-04-25 19:09:21 +0800

    修改

---
+++ index.js
@@ -0,0 +1,36 @@
+$(function(){
+
+		// 存储前一个显示的section
+	    var pre=$('.section').eq(0); 
+
+	    // 村吃前一个显示的section的索引值
+	    var preIndex=0;  
+
+	   	$('#menu li').hover(function(){
+	   		$('#menu li').attr('class','');
+	   		$(this).attr('class','present');
+
+	   		//获取当前section的索引
+	   		var index=$(this).index();
+
+	   		// 两种情况：从左向右滑出&从右向左滑出
+	   		// $('.section').eq(index).css('display','block');
+	   		if(preIndex>index){
+	   			pre.animate({left:'100%'}).css('zIndex',0);
+	   			$('.section').eq(index).css('left','-100%').css('zIndex',1);
+	   			$('.section').eq(index).animate({left:0});
+	   		}else if(preIndex<index){
+	   			pre.animate({left:'-100%'}).css('zIndex',0);
+		   		$('.section').eq(index).css('left','100%').css('zIndex',1);
+		   		$('.section').eq(index).animate({left:0});
+	   		}else if(preIndex==index){
+	   			$('.section').eq(index).css('left',0);
+	   		}
+	   		
+	   		pre=$('.section').eq(index);
+	   		preIndex=index;
+	   	},function(){
+
+	   	})
+
+})
