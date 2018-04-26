2c3a65901126d0b48ed27fa4c9e7cc9b7df69ed1
lymnkkk <843214245@qq.com>
2018-04-26 16:48:14 +0800

    兼容手机端，手机可左右滑动查看

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 $(function(){
-
 		// 存储前一个显示的section
 	    var pre=$('.section').eq(0); 
 
-	    // 村吃前一个显示的section的索引值
+	    // 存前一个显示的section的索引值
 	    var preIndex=0;  
 
-	   	$('#menu li').click(function(){
+	   	$('#menu li').hover(function(e){
 	   		$('#menu li').attr('class','');
 	   		$(this).attr('class','present');
 
@@ -14,21 +13,58 @@ $(function(){
 	   		var index=$(this).index();
 
 	   		// 两种情况：从左向右滑出&从右向左滑出
-	   		// $('.section').eq(index).css('display','block');
 	   		if(preIndex>index){
-	   			pre.animate({left:'100%'}).css('zIndex',0);
-	   			$('.section').eq(index).css('left','-100%').css('zIndex',1);
-	   			$('.section').eq(index).animate({left:0});
+	   			moveRight(pre,index);
 	   		}else if(preIndex<index){
-	   			pre.animate({left:'-100%'}).css('zIndex',0);
-		   		$('.section').eq(index).css('left','100%').css('zIndex',1);
-		   		$('.section').eq(index).animate({left:0});
+	   			moveLeft(pre,index);
 	   		}else if(preIndex==index){
 	   			$('.section').eq(index).css('left',0);
 	   		}
 	   		
 	   		pre=$('.section').eq(index);
 	   		preIndex=index;
+	   	},function(){
+	   		
 	   	})
+	   	
+		//手机屏幕左滑动
+		$(".section").on("swipeleft",function(){
+			var index=$(this).index()+1;
+			if(index>=$('.section').length) index=0;
+			
+			moveLeft(pre,index);		
+		   	$('#menu li').attr('class','');
+		   	$('#menu li').eq(index).attr('class','present');
+
+		   	pre=$('.section').eq(index);
+		   	preIndex=index;
+  		}); 
+
+		////手机屏幕右滑动
+ 		$(".section").on("swiperight",function(){
+			var index=$(this).index()-1;
+			if(index<0) index=$('.section').length-1;
+			
+    		moveRight(pre,index);
+		   	$('#menu li').attr('class','');
+		   	$('#menu li').eq(index).attr('class','present');
+
+		   	pre=$('.section').eq(index);
+		   	preIndex=index;
+  		}); 
+
+  		// moveLeft
+  		function moveLeft(pre,index){
+  			pre.animate({left:'-100%'}).css('zIndex',0);
+		   	$('.section').eq(index).css('left','100%').css('zIndex',1);
+		   	$('.section').eq(index).animate({left:0});
+  		}
+
+  		//moveRight
+  		function moveRight(pre,index){
+  			pre.animate({left:'100%'}).css('zIndex',0);
+	   		$('.section').eq(index).css('left','-100%').css('zIndex',1);
+	   		$('.section').eq(index).animate({left:0});
+  		}
 
 })
